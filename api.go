0728287fcf3cec532809dcc3ebc5dcda7d2e9119
morriswinkler <m.winkler@open3dengineering.org>
2014-12-06 18:44:13 +0100

    logon.js test

--- api.go
+++ api.go
@@ -3,8 +3,19 @@ package main
 import (
 	"./helpers"
 	"crypto/md5"
+	"encoding/json"
+	"github.com/garyburd/redigo/redis"
 	"strings"
 )
+
+type Space struct {
+	subnet []Player
+}
+
+type Player struct {
+	localIP  string
+	userName string
+}
 
 func Adduser(msg string) {
 	helpers.TRACE.Println("socket.io: adduser", msg)
@@ -12,10 +23,33 @@ func Adduser(msg string) {
 }
 
 func Logon(msg string) {
+
+	var space Space
+
 	ipNumbers := strings.Split(msg, " ")
 	helpers.TRACE.Println("socket.io: Logon", ipNumbers)
 
-	space := md5.Sum([]byte(ipNumbers[0]))
-	helpers.TRACE.Println("socket.io: Logon", space)
-
+	spaceID := md5.Sum([]byte(ipNumbers[1]))
+	helpers.TRACE.Println("socket.io: Logon", spaceID)
+	jsonSpace, err := redis.Bytes(RedisDB.Do("GET", spaceID))
+	if err != nil {
+		space := Space{
+			subnet: []Player{
+				{
+					localIP:  ipNumbers[0],
+					userName: "JonDoe",
+				},
+			},
+		}
+		jsonSpace, err := json.Marshal(space)
+		if err != nil {
+			ERROR.Println("socket.io->Logon error: ", err)
+		}
+		RedisDB.Do("SET", "spaceID", jsonSpace)
+	}
+	err = json.Unmarshal(jsonSpace, &space)
+	if err != nil {
+		ERROR.Println("socket.io->Logon error: ", err)
+	}
+	TRACE.Println("socket.io: ", space)
 }
