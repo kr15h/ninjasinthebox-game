f4ab725dc3c882c5fcf232e10b8945c087d67a79
morriswinkler <m.winkler@open3dengineering.org>
2014-12-06 18:48:53 +0100

    logon.js test

--- api.go
+++ api.go
@@ -3,8 +3,6 @@ package main
 import (
 	"./helpers"
 	"crypto/md5"
-	"encoding/json"
-	"github.com/garyburd/redigo/redis"
 	"strings"
 )
 
@@ -24,32 +22,9 @@ func Adduser(msg string) {
 
 func Logon(msg string) {
 
-	var space Space
-
 	ipNumbers := strings.Split(msg, " ")
 	helpers.TRACE.Println("socket.io: Logon", ipNumbers)
 
 	spaceID := md5.Sum([]byte(ipNumbers[1]))
 	helpers.TRACE.Println("socket.io: Logon", spaceID)
-	jsonSpace, err := redis.Bytes(RedisDB.Do("GET", spaceID))
-	if err != nil {
-		space := Space{
-			subnet: []Player{
-				{
-					localIP:  ipNumbers[0],
-					userName: "JonDoe",
-				},
-			},
-		}
-		jsonSpace, err := json.Marshal(space)
-		if err != nil {
-			ERROR.Println("socket.io->Logon error: ", err)
-		}
-		RedisDB.Do("SET", "spaceID", jsonSpace)
-	}
-	err = json.Unmarshal(jsonSpace, &space)
-	if err != nil {
-		ERROR.Println("socket.io->Logon error: ", err)
-	}
-	TRACE.Println("socket.io: ", space)
 }
