571b6bfa1a433cb88390ef517429994f0a0f7faa
morriswinkler <m.winkler@open3dengineering.org>
2014-12-06 11:30:46 +0100

    added socket.io

--- Readme.md
+++ Readme.md
@@ -6,6 +6,3 @@
 please send me your ssh key to m.winkler@laydrop.com
 
 than commit to ssh://morriswinkler@morriswinkler.koding.io
-
-
-there subdirectories in the /static please try your stuff inside them than merge it with /static
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"./helpers"
 	"flag"
+	"github.com/googollee/go-socket.io"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/pat"
 	"log"
@@ -82,17 +83,43 @@ func init() {
 	}
 
 }
-
 func main() {
+
+	// http API
 	router := pat.New()
+
+	// http static dir
 	router.Add("GET", "/", http.FileServer(http.Dir(cfg.Webserver.Dir)))
+
+	// websockt API
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server.On("connection", func(so socketio.Socket) {
+		TRACE.Println("socket.io: connection")
+		so.Join("chat")
+		so.On("chat message", func(msg string) {
+			TRACE.Println("socket.io->emit:", so.Emit("chat message", msg))
+			so.BroadcastTo("chat", "chat message", msg)
+		})
+		so.On("disconnection", func() {
+			TRACE.Println("socket.io: disconnect")
+		})
+	})
+	server.On("error", func(so socketio.Socket, err error) {
+		ERROR.Println("socket.io->error:", err)
+	})
+
+	http.Handle("/socket.io/", server)
 
 	// Register this pat with the default serve mux so that other packages
 	// may also be exported. (i.e. /debug/pprof[>)
 	http.Handle("/", handlers.CombinedLoggingHandler(helpers.LOGFILE, router))
 
 	INFO.Printf("listening on %s:%s", cfg.Webserver.Host, cfg.Webserver.Port)
-	err := http.ListenAndServe(cfg.Webserver.Host+":"+cfg.Webserver.Port, nil)
+	err = http.ListenAndServe(cfg.Webserver.Host+":"+cfg.Webserver.Port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
