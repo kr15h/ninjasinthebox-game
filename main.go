201401d0064469d468c26165ad08d2ca3dfd9468
morriswinkler <m.winkler@open3dengineering.org>
2014-12-07 02:19:45 +0100

    websocket debug

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"./helpers"
-	"code.google.com/p/go-uuid/uuid"
-	"encoding/json"
 	"flag"
 	"github.com/garyburd/redigo/redis"
 	"github.com/googollee/go-socket.io"
@@ -126,87 +124,7 @@ func main() {
 			TRACE.Println("socket.io: disconnect", so.Id(), so.Request())
 		})
 		so.On("adduser", Adduser)
-		so.On("logon", func(so socketio.Socket, msg string) {
-
-			var space Space
-			var known bool = false
-
-			ipNumbers := strings.Split(msg, " ")
-			helpers.TRACE.Println("socket.io->Logon: IP", ipNumbers)
-
-			localIP := ipNumbers[0]
-			spaceID := ipNumbers[1]
-			helpers.TRACE.Println("socket.io->Logon: SpaceID", spaceID)
-
-			redisDB := RedisPool.Get()
-			defer redisDB.Close()
-
-			jsonSpace, err := redis.Bytes(redisDB.Do("GET", spaceID))
-			if err != nil {
-				// so the user is in a new space we add him
-				known = true
-				TRACE.Println("socket.io->Logon: newSpace", err)
-				space = Space{
-					Channel: uuid.New(),
-					SpaceID: spaceID,
-					Space: []Player{
-						{
-							LocalIP:  localIP,
-							UserName: "JonDoe",
-						},
-					},
-				}
-				jsonSpace, err := json.Marshal(space)
-				if err != nil {
-					ERROR.Println("socket.io->Logon json.Marshal error: ", err)
-				}
-				_, err = redisDB.Do("SET", spaceID, jsonSpace)
-				if err != nil {
-					ERROR.Println("socket.io->Logon RedisDB SET error: ", err)
-				}
-
-			} else {
-				// else unmarshal the json object
-				err = json.Unmarshal(jsonSpace, &space)
-				if err != nil {
-					ERROR.Println("socket.io->Logon json.Unmarshal error: ", err)
-				}
-			}
-
-			// check if the user is known
-			for _, element := range space.Space {
-				if element.LocalIP == localIP {
-					known = true
-					TRACE.Println("socket.io->Logon known LocalIP", element.LocalIP, "in Space", spaceID)
-				}
-			}
-
-			// if ip is unknow add it to the space
-			if !known {
-				TRACE.Println("socket.io->Logon unknown LocalIP", localIP, "is added")
-
-				player := Player{
-					LocalIP:  localIP,
-					UserName: "JonDoe",
-				}
-
-				space.Space = append(space.Space, player)
-				jsonSpace, err := json.Marshal(space)
-				if err != nil {
-					ERROR.Println("socket.io->Logon json.Marshal error: ", err)
-				}
-				_, err = redisDB.Do("SET", spaceID, jsonSpace)
-				if err != nil {
-					ERROR.Println("socket.io->Logon RedisDB SET error: ", err)
-				}
-
-				TRACE.Println("socket.io->Logon added", space)
-			}
-
-			TRACE.Println("socket.io->Logon Emit Answer with Id", so.Id(), space)
-			so.Emit("space", space)
-			so.BroadcastTo(so.Id(), "space", space)
-		})
+		so.On("logon", Logon)
 		so.On("joinGame", JoinGame)
 
 	})
